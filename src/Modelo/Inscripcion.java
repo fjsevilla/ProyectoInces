3dc667d4f5b269036e995317cebf564e87a46f66
Doris <Doris@192.168.10.107>
2015-11-07 18:10:51 -0430

    Agregando mas ventanas

--- src/Modelo/Inscripcion.java
+++ src/Modelo/Inscripcion.java
@@ -6,6 +6,7 @@
 package Modelo;
 
 import java.awt.Container;
+import java.awt.GridLayout;
 
 /**
  *
@@ -13,10 +14,47 @@ import java.awt.Container;
  */
 public class Inscripcion extends Container{
     
-    CampoTexto cedula;
+    CampoTexto cedula, nombre, apellido;
+    PanelJComboBox area, curso, horarios;
+    Container persona, cursos;
     
     public Inscripcion(){
-        cedula = new CampoTexto(10, "Cedula");
+        setLayout(new GridLayout(2,1));
+        add(panelSuperior());
+        add(panelInferior());
+        
+        
+    }
+    
+    private Container panelSuperior(){
+        cedula = new CampoTexto(10, "Cedula: ");
+        nombre = new CampoTexto(30, "Nombres: ");
+        apellido = new CampoTexto(30,  "Apellidos: ");
+        
+        nombre.hacerEditable(false);
+        apellido.hacerEditable(false);
+        
+        persona = new Container();
+        persona.setLayout(new GridLayout(3,1));
+        persona.add(cedula);
+        persona.add(nombre);
+        persona.add(apellido);
+        
+        return persona;
+    }
+    
+    private Container panelInferior(){
+        area = new PanelJComboBox("Area: ");
+        curso = new PanelJComboBox("Curso: ");
+        horarios = new PanelJComboBox("Turno: ");
+        
+        cursos = new Container();
+        cursos.setLayout(new GridLayout(1,3));
+        cursos.add(area);
+        cursos.add(curso);
+        cursos.add(horarios);
+        
+        return cursos;
     }
     
 }
