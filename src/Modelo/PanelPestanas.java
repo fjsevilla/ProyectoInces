a73a68cc982ae329727a9f38e3b3bf5dc96ad5f0
Doris <Doris@192.168.10.107>
2015-11-07 19:25:56 -0430

    Agregando mas ventanas

---
+++ src/Modelo/PanelPestanas.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Modelo;
+
+import javax.swing.JTabbedPane;
+
+/**
+ *
+ * @author Yuli / Fr@n
+ */
+public class PanelPestanas extends JTabbedPane{
+    
+    PanelPersona persona;
+    Cursos cursos;
+    Inscripcion inscripcion;
+    
+    public PanelPestanas(){
+        persona = new PanelPersona();
+        cursos = new Cursos();
+        inscripcion = new Inscripcion();
+        
+        this.addTab("Sujeto", persona);
+        this.addTab("Cursos", cursos);
+        this.addTab("Inscripcion", inscripcion);
+    }
+    
+}
